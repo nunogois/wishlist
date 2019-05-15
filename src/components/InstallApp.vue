f66868994909be4b3328f46da721781d80c80da3
Nuno Góis <nunogois@iqa.pt>
2019-05-15 12:52:26 +0100

    install pwa component

---
+++ src/components/InstallApp.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card class="q-mx-md q-mt-sm" dense v-if="show_install">
+    <q-card-main>
+      Would you like to install Wishlist?
+      <q-btn class="on-right" dense label="Install" flat color="primary" @click.prevent="install"/>
+    </q-card-main>
+  </q-card>
+</template>
+
+<script>
+let installEvent;
+
+export default {
+  data() {
+    return {
+      show_install: false
+    };
+  },
+  created() {
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      installEvent = e;
+      this.showInstallBanner = true;
+    });
+  },
+  methods: {
+    install() {
+      this.showInstallBanner = false;
+      installEvent.prompt();
+      installEvent.userChoice.then(() => {
+        installEvent = null;
+      });
+    }
+  }
+};
+</script>
