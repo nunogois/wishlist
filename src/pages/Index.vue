b3de8c5257cfc62d4053fa65b1d8a95fcd7d6061
Nuno Góis <nunojosegois@gmail.com>
2019-05-22 11:36:27 +0100

    version

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -50,7 +50,7 @@
         Thank you for using Wishlist!
       </p>
       <div slot="body">
-        <span>1.19.05.17.1800</span>
+        <span>1.19.05.22.1135</span>
       </div>
     </q-dialog>
   </q-page>
