612a6532c8ccfaf04b39939e5c30444c5ee46522
Nuno Góis <nunojosegois@gmail.com>
2019-05-24 18:05:18 +0100

    version bump

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -71,7 +71,7 @@
         Thank you for using Wishlist!
       </p>
       <div slot="body">
-        <span>1.19.05.24.1700</span>
+        <span>1.19.05.24.1800</span>
       </div>
     </q-dialog>
 
