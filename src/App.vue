099f42039dc4ac7388023eae96127d699c3bce7e
Nuno Góis <nunojosegois@gmail.com>
2019-05-24 16:02:25 +0100

    updated readme + added thumbnail

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
       og_title: { prefix: 'og: http://ogp.me/ns#', property: 'og:title', content: 'Wishlist' },
       og_website: { prefix: 'og: http://ogp.me/ns# website: http://ogp.me/ns/website#', property: 'og:type', content: 'website' },
       og_description: { prefix: 'og: http://ogp.me/ns#', property: 'og:description', content: 'Wishlist' },
-      og_image: { prefix: 'og: http://ogp.me/ns#', property: 'og:image', content: 'https://wishlist-quasar.netlify.com/thumbnail.png' },
+      og_image: { prefix: 'og: http://ogp.me/ns#', property: 'og:image', content: 'https://wishlist-quasar.netlify.com/statics/img/thumbnail.png' },
       og_url: { prefix: 'og: http://ogp.me/ns#', property: 'og:url', content: 'https://wishlist-quasar.netlify.com/' },
       twitter_card: { name: 'twitter:card', content: 'summary_large_image' },
       theme_color: { name: 'theme-color', content: '#000000'}
